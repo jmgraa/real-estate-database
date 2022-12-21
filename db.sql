5723aa6044c6852704e23e4520038f2d61a2c571
Jakub Magiera <jmgra02@gmail.com>
2022-12-21 21:36:38 +0100

    Działająca kwerenda

--- db.sql
+++ db.sql
@@ -1,3 +1,33 @@
+IF OBJECT_ID('Cechy_nieruchomości','U') IS NOT NULL
+	DROP TABLE Cechy_nieruchomości
+
+IF OBJECT_ID('Terminy_oglądania','U') IS NOT NULL
+	DROP TABLE Terminy_oglądania
+
+IF OBJECT_ID('Oferty_kupna', 'U') IS NOT NULL
+    DROP TABLE Oferty_kupna
+
+IF OBJECT_ID('Sprzedane_nieruchomości') IS NOT NULL
+    DROP TABLE Sprzedane_nieruchomości
+
+IF OBJECT_ID('Trendy_rynkowe','U') IS NOT NULL
+	DROP TABLE Trendy_rynkowe
+
+IF OBJECT_ID('Rezerwacje','U') IS NOT NULL
+	DROP TABLE Rezerwacje
+
+IF OBJECT_ID('Umowy','U') IS NOT NULL
+	DROP TABLE Umowy
+
+IF OBJECT_ID('Nieruchomości','U') IS NOT NULL
+	DROP TABLE Nieruchomości
+
+IF OBJECT_ID('Klienci','U') IS NOT NULL
+	DROP TABLE Klienci
+
+IF OBJECT_ID('Pracownicy','U') IS NOT NULL
+	DROP TABLE Pracownicy
+
 CREATE TABLE Nieruchomości (
 	ID_nieruchomości INT PRIMARY KEY,
     Rynek VARCHAR(9)
@@ -32,8 +62,7 @@ CREATE TABLE Pracownicy (
 CREATE TABLE Cechy_nieruchomości (
     ID_nieruchomości INT,
     Nazwa_cechy VARCHAR(200) NOT NULL,
-    FOREIGN KEY ID_nieruchomości REFERENCES Nieruchomości(ID_nieruchomości),
-    FOREIGN KEY ID_cechy REFERENCES Cechy(ID_cechy)
+    FOREIGN KEY (ID_nieruchomości) REFERENCES Nieruchomości(ID_nieruchomości)
 )
 
 CREATE TABLE Terminy_oglądania (
@@ -41,8 +70,8 @@ CREATE TABLE Terminy_oglądania (
     Id_zwiedzającego INT,
     Data_zwiedzania DATETIME NOT NULL,
     Miejsce_zwiedzania INT,
-    FOREIGN KEY Id_zwiedzającego REFERENCES ID_klienta,    
-    FOREIGN KEY Miejsce_zwiedzania REFERENCES Nieruchomości(ID_nieruchomości)
+    FOREIGN KEY (Id_zwiedzającego) REFERENCES Klienci(ID_klienta),    
+    FOREIGN KEY (Miejsce_zwiedzania) REFERENCES Nieruchomości(ID_nieruchomości)
 )
 
 CREATE TABLE Oferty_kupna (
@@ -51,8 +80,8 @@ CREATE TABLE Oferty_kupna (
     Pracownik_obsługujący INT,
     Data_wystawienia DATETIME NOT NULL,
     Data_zakończenia DATETIME NOT NULL,
-    FOREIGN KEY ID_nieruchomości REFERENCES Nieruchomości(ID_nieruchomości),
-    FOREIGN KEY Pracownik_obsługujący REFERENCES Pracownicy(ID_pracownika)
+    FOREIGN KEY (ID_nieruchomości) REFERENCES Nieruchomości(ID_nieruchomości),
+    FOREIGN KEY (Pracownik_obsługujący) REFERENCES Pracownicy(ID_pracownika),
 
     CHECK (Data_wystawienia < Data_zakończenia)
 )
@@ -64,9 +93,9 @@ CREATE TABLE Sprzedane_nieruchomości (
     ID_kupującego INT,
     Data_sprzedania DATETIME NOT NULL,    
     Mnożnik_ceny FLOAT DEFAULT 1,
-    FOREIGN KEY ID_nieruchomości REFERENCES Nieruchomości(ID_nieruchomości),
-    FOREIGN KEY Pracownik_obsługujący REFERENCES Pracownicy(ID_pracownika),
-    FOREIGN KEY ID_kupującego REFERENCES Pracownicy(ID_pracownika)
+    FOREIGN KEY (ID_nieruchomości) REFERENCES Nieruchomości(ID_nieruchomości),
+    FOREIGN KEY (Pracownik_obsługujący) REFERENCES Pracownicy(ID_pracownika),
+    FOREIGN KEY (ID_kupującego) REFERENCES Pracownicy(ID_pracownika)
 )
 
 CREATE TABLE Trendy_rynkowe (
@@ -75,7 +104,6 @@ CREATE TABLE Trendy_rynkowe (
     Zakończenie DATETIME,
     Region VARCHAR(200),
     Zmiana_mnożnika FLOAT NOT NULL,
-    FOREIGN KEY Region REFERENCES Nieruchomości(Nazwa_regionu),
 
     CHECK (Rozpoczęcie < Zakończenie)
 )
@@ -85,8 +113,8 @@ CREATE TABLE Rezerwacje (
     ID_klienta INT,
     Początek DATETIME NOT NULL,
     Koniec DATETIME NOT NULL,
-    FOREIGN KEY ID_nieruchomości REFERENCES Nieruchomości(ID_nieruchomości),
-    FOREIGN KEY ID_klienta REFERENCES Klienci(ID_klienta),
+    FOREIGN KEY (ID_nieruchomości) REFERENCES Nieruchomości(ID_nieruchomości),
+    FOREIGN KEY (ID_klienta) REFERENCES Klienci(ID_klienta),
 
     CHECK (Początek < Koniec)
 )
@@ -97,9 +125,9 @@ CREATE TABLE Umowy (
     ID_nieruchomości INT,
     Początek_najmu DATETIME NOT NULL,
     Koniec_najmu DATETIME NOT NULL,
-    FOREIGN KEY ID_klienta REFERENCES Klienci(ID_klienta),
-    FOREIGN KEY ID_pracownika REFERENCES Pracownicy(ID_pracownika),
-    FOREIGN KEY ID_nieruchomości REFERENCES Nieruchomości(ID_nieruchomości),
+    FOREIGN KEY (ID_klienta) REFERENCES Klienci(ID_klienta),
+    FOREIGN KEY (ID_pracownika) REFERENCES Pracownicy(ID_pracownika),
+    FOREIGN KEY (ID_nieruchomości) REFERENCES Nieruchomości(ID_nieruchomości),
 
     CHECK (Początek_najmu < Koniec_najmu)
 )
