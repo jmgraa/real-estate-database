239399edafa04e7e6712339643b89b73a97894d9
jmgra <jmgra02@gmail.com>
2023-01-05 20:43:56 +0100

    Automatyczne numerowanie id

--- procedury.sql
+++ procedury.sql
@@ -11,38 +11,38 @@ IF OBJECT_ID('DodajNieruchomość', 'P') IS NOT NULL
     DROP PROCEDURE DodajNieruchomość
 GO
 
-CREATE PROCEDURE DodajDom (@Id INT, @Type VARCHAR(200), @Rooms INT, @Floors INT, @Heating VARCHAR(200))
+CREATE PROCEDURE DodajDom (@Type VARCHAR(200), @Rooms INT, @Floors INT, @Heating VARCHAR(200))
 AS
-INSERT INTO Domy VALUES (@Id, @Type, @Rooms, @Floors, @Heating)
+INSERT INTO Domy VALUES (@Type, @Rooms, @Floors, @Heating)
 GO
 
-CREATE PROCEDURE DodajMieszkanie @Id INT, @Type VARCHAR(200), @Floor INT, @Heating BIT, @Lift BIT
+CREATE PROCEDURE DodajMieszkanie @Type VARCHAR(200), @Floor INT, @Heating BIT, @Lift BIT
 AS
 INSERT INTO Mieszkania VALUES
-(@Id, @Type, @Floor, @Heating, @Lift)
+(@Type, @Floor, @Heating, @Lift)
 GO
 
-CREATE PROCEDURE DodajDziałkę @Id INT, @Type VARCHAR(200), @Electricty BIT, @Gas BIT, @Water BIT, @Sewers BIT
+CREATE PROCEDURE DodajDziałkę @Type VARCHAR(200), @Electricty BIT, @Gas BIT, @Water BIT, @Sewers BIT
 AS
 INSERT INTO Działki VALUES
-(@Id, @Type, @Electricty, @Gas, @Water, @Sewers)
+(@Type, @Electricty, @Gas, @Water, @Sewers)
 GO
 
-CREATE PROCEDURE DodajNieruchomość (@Id INT, @Type_of_estate VARCHAR(200), @Street VARCHAR(20), @Number INT, @Place VARCHAR(200), @Space INT, @Price INT, @Negotiable BIT) 
+CREATE PROCEDURE DodajNieruchomość (@Type_of_estate VARCHAR(200), @Street VARCHAR(20), @Number INT, @Place VARCHAR(200), @Space INT, @Price INT, @Negotiable BIT) 
 AS
 INSERT INTO Nieruchomości VALUES
-(@Id, @Street, @Number, @Place, @Space, @Price, @Negotiable)
+(@Street, @Number, @Place, @Space, @Price, @Negotiable)
 
 IF @Type_of_estate = 'dom' BEGIN
-    EXEC DodajDom @Id, 'pies', 2, 3, 'kot'
+    EXEC DodajDom 'pies', 2, 3, 'kot'
     PRINT('Dodano ogłoszenie domu!')
 END
 ELSE IF @Type_of_estate = 'mieszkanie' BEGIN
-    EXEC DodajMieszkanie @Id, 'słoń', 1, 0, 1
+    EXEC DodajMieszkanie 'słoń', 1, 0, 1
     PRINT('Dodano ogłoszenie mieszkania!')
 END
 ELSE IF @Type_of_estate = 'działka' BEGIN
-    EXEC DodajDziałkę @Id, 'kura', 1, 0, 0, 1
+    EXEC DodajDziałkę 'kura', 1, 0, 0, 1
     PRINT('Dodano ogłoszenie działki!')
 END
 ELSE BEGIN
