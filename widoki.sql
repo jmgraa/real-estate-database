3ec0e675da7e79252028bb359f168ecc34f5fd4b
KonradSitek <69002597+KonradSitek@users.noreply.github.com>
2023-01-06 12:08:05 +0100

    Create widoki.sql

--- widoki.sql
+++ widoki.sql
@@ -1,1 +1,11 @@
 
+CREATE VIEW Ranking_pracowników AS
+SELECT Os.Imię, Os.Nazwisko, P.ID_pracownika, AVG(O.Ocena) AS Średnia_ocena FROM Opinie O
+INNER JOIN Wszystkie_oferty W ON
+W.ID_oferty = O.ID_Oferty
+INNER JOIN Pracownicy P ON
+W.Pracownik_obsługujący = P.ID_pracownika
+INNER JOIN Osoby Os ON
+Os.Pesel = P.ID_pracownika
+GROUP BY P.ID_pracownika,Os.Imię, Os.Nazwisko
+ORDER BY Średnia_ocena DESC
