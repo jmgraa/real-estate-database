3bf3e1bfc0804072240b9906978292b24fabb378
KonradSitek <69002597+KonradSitek@users.noreply.github.com>
2023-01-06 10:28:52 +0100

    Update wyzwalacze.sql

--- wyzwalacze.sql
+++ wyzwalacze.sql
@@ -10,3 +10,18 @@ BEGIN
 		FROM inserted;
     END;
 END;
+
+
+
+CREATE TRIGGER Dodaj_do_niesprzedane
+ON Wszystkie_oferty
+AFTER INSERT
+AS
+BEGIN
+    IF (EXISTS(SELECT 1 FROM inserted WHERE Data_zakończenia > GETDATE()))
+	BEGIN
+        INSERT INTO Niesprzedane(ID_niesprzedane)
+		SELECT ID_oferty
+		FROM inserted;
+    END;
+END;
