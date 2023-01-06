bc03e8d36ef0e6207e5ab103da7cd7e303cc52db
KonradSitek <69002597+KonradSitek@users.noreply.github.com>
2023-01-06 10:13:46 +0100

    Create wyzwalacze.sql

---
+++ wyzwalacze.sql
@@ -0,0 +1,12 @@
+CREATE TRIGGER Dodaj_do_aktualne
+ON Wszystkie_oferty
+AFTER INSERT
+AS
+BEGIN
+    IF (EXISTS(SELECT 1 FROM inserted WHERE Data_wystawienia < GETDATE()))
+	BEGIN
+        INSERT INTO Aktualne_oferty(ID_aktualne)
+		SELECT ID_oferty
+		FROM inserted;
+    END;
+END;
