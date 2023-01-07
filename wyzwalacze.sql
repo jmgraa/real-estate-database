f88157b06495a912da110c03fca2bc2eca37bd0b
KonradSitek <69002597+KonradSitek@users.noreply.github.com>
2023-01-07 12:04:26 +0100

    Update wyzwalacze.sql

--- wyzwalacze.sql
+++ wyzwalacze.sql
@@ -27,7 +27,7 @@ BEGIN
 END;
 
 
-CREATE TRIGGER Aktualizuj_ceny
+CREATE TRIGGER Aktualizuj_ceny2
 ON Trendy_rynkowe
 AFTER INSERT
 AS
@@ -37,12 +37,21 @@ BEGIN
   FROM nieruchomości
   INNER JOIN inserted i
     ON nieruchomości.Miejscowość = i.Miejscowość
-  WHERE i.Nazwa_trendu = 'wzrost'
+  WHERE i.Nazwa_trendu = 'wzrost' AND i.Rozpoczęcie < GETDATE() AND i.Zakończenie > GETDATE()
 
   UPDATE nieruchomości
   SET nieruchomości.Cena = nieruchomości.Cena - nieruchomości.Cena * i.Zmiana_Mnożnika
   FROM nieruchomości
   INNER JOIN inserted i
     ON nieruchomości.Miejscowość = i.Miejscowość
-  WHERE i.Nazwa_trendu = 'spadek'
+  WHERE i.Nazwa_trendu = 'spadek' AND i.Rozpoczęcie < GETDATE() AND i.Zakończenie > GETDATE()
 END
+
+
+CREATE TRIGGER Usun_z_aktualne
+ON Rezerwacje
+AFTER INSERT
+AS
+DELETE FROM Aktualne_oferty
+WHERE Aktualne_oferty.ID_aktualne = (SELECT Rezerwacje.ID_nieruchomości FROM inserted i JOIN Rezerwacje ON i.ID_nieruchomości = Rezerwacje.ID_nieruchomości)
+GO
