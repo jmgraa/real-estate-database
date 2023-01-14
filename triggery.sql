660f84d2382c6ab07f2c14c4b90d77df306859b0
jmgra <jmgra02@gmail.com>
2023-01-14 20:33:49 +0100

    Poprawienie wszystkiego, wprowadzenie pętli

--- triggery.sql
+++ triggery.sql
@@ -22,21 +22,30 @@ ON Trendy_rynkowe
 AFTER INSERT
 AS
 BEGIN
-    DECLARE @multiplier FLOAT = (SELECT Zmiana_Mnożnika FROM INSERTED)
-    DECLARE @place VARCHAR(MAX) = (SELECT Miejscowość FROM INSERTED)
+    DECLARE @loop_border INT = (SELECT MAX(ID_trendu) FROM INSERTED)
+    DECLARE @iterator INT = (SELECT MIN(ID_trendu) FROM INSERTED)
 
-    IF ((SELECT Nazwa_trendu FROM INSERTED) = 'wzrost')
-    BEGIN
-        UPDATE Nieruchomości        
-            SET Nieruchomości.Cena = Nieruchomości.Cena * (1 + @multiplier) 
-            WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
-    END
+    WHILE(@iterator <= @loop_border) BEGIN
+        IF EXISTS(SELECT ID_trendu FROM INSERTED WHERE ID_trendu = @iterator) BEGIN
+            DECLARE @multiplier FLOAT = (SELECT Zmiana_Mnożnika FROM INSERTED WHERE ID_trendu = @iterator)
+            DECLARE @place VARCHAR(MAX) = (SELECT Miejscowość FROM INSERTED WHERE ID_trendu = @iterator)
 
-    ELSE IF ((SELECT Nazwa_trendu FROM INSERTED) = 'spadek')
-    BEGIN
-        UPDATE Nieruchomości
-            SET Nieruchomości.Cena = Nieruchomości.Cena * (1 - @multiplier) 
-            WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
+            IF ((SELECT Nazwa_trendu FROM INSERTED WHERE ID_trendu = @iterator) = 'wzrost')
+            BEGIN
+                UPDATE Nieruchomości        
+                    SET Nieruchomości.Cena = Nieruchomości.Cena * (1 + @multiplier) 
+                    WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
+            END
+
+            ELSE IF ((SELECT Nazwa_trendu FROM INSERTED WHERE ID_trendu = @iterator) = 'spadek')
+            BEGIN
+                UPDATE Nieruchomości
+                    SET Nieruchomości.Cena = Nieruchomości.Cena * (1 - @multiplier)
+                    WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
+            END
+        END
+
+        SET @iterator = @iterator + 1           
     END
 END
 GO
@@ -46,39 +55,57 @@ ON Trendy_rynkowe
 AFTER DELETE
 AS
 BEGIN
-    DECLARE @multiplier FLOAT = (SELECT Zmiana_Mnożnika FROM DELETED)
-    DECLARE @place VARCHAR(MAX) = (SELECT Miejscowość FROM DELETED)
+    DECLARE @loop_border INT = (SELECT MAX(ID_trendu) FROM DELETED)
+    DECLARE @iterator INT = (SELECT MIN(ID_trendu) FROM DELETED)
 
-    IF ((SELECT Nazwa_trendu FROM DELETED) = 'wzrost')
-    BEGIN
-        UPDATE Nieruchomości        
-            SET Nieruchomości.Cena = Nieruchomości.Cena * 100 / (1 + @multiplier) 
-            WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
-    END
+    WHILE(@iterator <= @loop_border) BEGIN
+        IF EXISTS(SELECT ID_trendu FROM DELETED WHERE ID_trendu = @iterator) BEGIN
+            DECLARE @multiplier FLOAT = (SELECT Zmiana_Mnożnika FROM DELETED WHERE ID_trendu = @iterator)
+            DECLARE @place VARCHAR(MAX) = (SELECT Miejscowość FROM DELETED WHERE ID_trendu = @iterator)
 
-    ELSE IF ((SELECT Nazwa_trendu FROM DELETED) = 'spadek')
-    BEGIN
-        UPDATE Nieruchomości
-            SET Nieruchomości.Cena = Nieruchomości.Cena * 100 / (1 - @multiplier)
-            WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
+            IF ((SELECT Nazwa_trendu FROM DELETED WHERE ID_trendu = @iterator) = 'wzrost')
+            BEGIN
+                UPDATE Nieruchomości        
+                    SET Nieruchomości.Cena = Nieruchomości.Cena * 100 / (1 + @multiplier) 
+                    WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
+            END
+
+            ELSE IF ((SELECT Nazwa_trendu FROM DELETED WHERE ID_trendu = @iterator) = 'spadek')
+            BEGIN
+                UPDATE Nieruchomości
+                    SET Nieruchomości.Cena = Nieruchomości.Cena * 100 / (1 - @multiplier)
+                    WHERE Nieruchomości.Miejscowość = @place AND Nieruchomości.ID_nieruchomości IN (SELECT ID_aktualne FROM Aktualne)
+            END
+        END
+
+        SET @iterator = @iterator + 1           
     END
 END
 GO
 
 CREATE TRIGGER PrzydzieleniePracownika
-ON Wszystkie_oferty
+ON Aktualne
 AFTER INSERT
 AS
 BEGIN
-    DECLARE @employee INT = (SELECT TOP 1 ID_pracownika FROM Pracownicy ORDER BY Liczba_aktualnych_zleceń ASC)
+    DECLARE @loop_border INT = (SELECT MAX(ID_aktualne) FROM INSERTED)
+    DECLARE @iterator INT = (SELECT MIN(ID_aktualne) FROM INSERTED)
 
-    UPDATE Pracownicy
-        SET Liczba_aktualnych_zleceń = Liczba_aktualnych_zleceń + 1
-        WHERE ID_pracownika = @employee
+    WHILE(@iterator <= @loop_border) BEGIN
+        IF EXISTS(SELECT ID_aktualne FROM INSERTED WHERE ID_aktualne = @iterator) BEGIN
+            DECLARE @employee VARCHAR(11) = (SELECT TOP 1 ID_pracownika FROM Pracownicy ORDER BY Liczba_aktualnych_zleceń ASC)
 
-    UPDATE Wszystkie_oferty
-        SET Pracownik_obsługujący = @employee
-        WHERE ID_oferty = (SELECT TOP 1 ID_oferty FROM Wszystkie_oferty ORDER BY ID_oferty DESC)
+            UPDATE Pracownicy
+                SET Liczba_aktualnych_zleceń = Liczba_aktualnych_zleceń + 1
+                WHERE ID_pracownika = @employee
+
+            UPDATE Wszystkie_oferty
+                SET Pracownik_obsługujący = @employee
+                WHERE ID_oferty = @iterator
+        END
+
+        SET @iterator = @iterator + 1           
+    END
 END
 GO
 
@@ -87,11 +114,20 @@ ON Sprzedane
 AFTER INSERT
 AS
 BEGIN
-    DECLARE @employee INT = (SELECT Pracownik_obsługujący FROM INSERTED INNER JOIN Wszystkie_oferty ON INSERTED.ID_sprzedane = Wszystkie_oferty.ID_oferty)
+    DECLARE @loop_border INT = (SELECT MAX(ID_sprzedane) FROM INSERTED)
+    DECLARE @iterator INT = (SELECT MIN(ID_sprzedane) FROM INSERTED)
 
-    UPDATE Pracownicy
-        SET Liczba_aktualnych_zleceń = Liczba_aktualnych_zleceń - 1
-        WHERE ID_pracownika = @employee
+    WHILE(@iterator <= @loop_border) BEGIN
+        IF EXISTS(SELECT ID_sprzedane FROM INSERTED WHERE ID_sprzedane = @iterator) BEGIN
+            DECLARE @employee VARCHAR(11) = (SELECT Pracownik_obsługujący FROM INSERTED INNER JOIN Wszystkie_oferty ON INSERTED.ID_sprzedane = Wszystkie_oferty.ID_oferty WHERE INSERTED.ID_sprzedane = @iterator)
+
+            UPDATE Pracownicy
+                SET Liczba_aktualnych_zleceń = Liczba_aktualnych_zleceń - 1
+                WHERE ID_pracownika = @employee
+        END
+
+        SET @iterator = @iterator + 1           
+    END
 END
 GO
 
@@ -100,11 +136,20 @@ ON Niesprzedane
 AFTER INSERT
 AS
 BEGIN
-    DECLARE @employee INT = (SELECT Pracownik_obsługujący FROM INSERTED INNER JOIN Wszystkie_oferty ON INSERTED.ID_niesprzedane = Wszystkie_oferty.ID_oferty)
+    DECLARE @loop_border INT = (SELECT MAX(ID_niesprzedane) FROM INSERTED)
+    DECLARE @iterator INT = (SELECT MIN(ID_niesprzedane) FROM INSERTED)
 
-    UPDATE Pracownicy
-        SET Liczba_aktualnych_zleceń = Liczba_aktualnych_zleceń - 1
-        WHERE ID_pracownika = @employee
+    WHILE(@iterator <= @loop_border) BEGIN
+        IF EXISTS(SELECT ID_niesprzedane FROM INSERTED WHERE ID_niesprzedane = @iterator) BEGIN
+            DECLARE @employee VARCHAR(11) = (SELECT Pracownik_obsługujący FROM INSERTED INNER JOIN Wszystkie_oferty ON INSERTED.ID_niesprzedane = Wszystkie_oferty.ID_oferty WHERE INSERTED.ID_niesprzedane = @iterator)
+
+            UPDATE Pracownicy
+                SET Liczba_aktualnych_zleceń = Liczba_aktualnych_zleceń - 1
+                WHERE ID_pracownika = @employee
+        END
+
+        SET @iterator = @iterator + 1           
+    END
 END
 GO
 
