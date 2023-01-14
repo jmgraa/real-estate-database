77d2e012bc17c1895838ad2c348ce6fa93ad0352
KonradSitek <69002597+KonradSitek@users.noreply.github.com>
2023-01-15 00:00:26 +0100

    Update widoki.sql

--- widoki.sql
+++ widoki.sql
@@ -1,24 +1,60 @@
 IF OBJECT_ID('Ranking_pracowników', 'V') IS NOT NULL
     DROP VIEW Ranking_pracowników
 GO
+
 IF OBJECT_ID('Liczba_ofert_w_miesiącu', 'V') IS NOT NULL
     DROP VIEW Liczba_ofert_w_miesiącu
 GO
+
 IF OBJECT_ID('Suma_wartości', 'V') IS NOT NULL
     DROP VIEW Suma_wartości
 GO
+
 IF OBJECT_ID('Liczba_terminów_pracowników', 'V') IS NOT NULL
     DROP VIEW Liczba_terminów_pracowników
 GO
+
 IF OBJECT_ID('Obrót_pracowników', 'V') IS NOT NULL
     DROP VIEW Obrót_pracowników
 GO
+
 IF OBJECT_ID('Aktualne_z_miasta', 'IF') IS NOT NULL
     DROP FUNCTION Aktualne_z_miasta
 GO
-IF OBJECT_ID('Oferty_typu', 'TF') IS NOT NULL
+
+IF OBJECT_ID('Oferty_typu', 'IF') IS NOT NULL
     DROP FUNCTION Oferty_typu
 GO
+
+IF OBJECT_ID('Ofety_od_do','IF') IS NOT NULL
+    DROP FUNCTION Oferty_od_do
+ GO
+ 
+ IF OBJECT_ID('Info_oferta', 'IF') IS NOT NULL
+	DROP FUNCTION Info_oferta
+GO
+
+IF OBJECT_ID('Info_oferta', 'IF') IS NOT NULL
+	DROP FUNCTION Info_oferta
+	
+	
+IF OBJECT_ID('Pracownik_aktualne', 'V') IS NOT NULL
+    DROP VIEW Pracownik_aktualne
+GO
+
+IF OBJECT_ID('Pracownik_sprzedane', 'V') IS NOT NULL
+    DROP VIEW Pracownik_sprzedane
+GO
+
+
+IF OBJECT_ID('Pracownik_niesprzedane', 'V') IS NOT NULL
+    DROP VIEW Pracownik_niesprzedane
+GO
+
+IF OBJECT_ID('Pracownik_statystyki', 'V') IS NOT NULL
+    DROP VIEW Pracownik_statystyki
+GO
+
 
 CREATE VIEW Ranking_pracowników AS
 	SELECT Os.Imię, Os.Nazwisko, P.ID_pracownika, AVG(O.Ocena) AS Średnia_ocena FROM Opinie O
@@ -90,3 +126,65 @@ BEGIN
 	RETURN
 END
 GO
+
+
+
+CREATE FUNCTION Oferty_od_do(@a INT, @b INT)
+RETURNS TABLE
+AS
+RETURN
+	SELECT * FROM Nieruchomości WHERE Nieruchomości.Cena >= @a AND Nieruchomości.Cena <= @b;
+GO
+
+
+CREATE FUNCTION Info_oferta(@x INT)
+RETURNS TABLE
+AS
+RETURN 
+	SELECT 
+	W.Pracownik_obsługujący, W.Data_wystawienia, W.Data_zakończenia, N.Ulica, N.Numer,N.Miejscowość,N.Powierzchnia, N.Cena,N.Możliwość_negocjacji_ceny FROM Wszystkie_oferty W INNER JOIN
+	Nieruchomości N ON
+	W.ID_nieruchomości = N.ID_nieruchomości
+	WHERE W.ID_nieruchomości = @x
+GO
+
+CREATE VIEW Pracownik_aktualne AS
+	SELECT O.Pesel, COUNT(A.ID_aktualne) AS [Ilosc aktualnych] FROM Osoby O
+	INNER JOIN Wszystkie_oferty W ON
+	W.Pracownik_obsługujący = O.Pesel
+	LEFT JOIN
+	Aktualne_oferty A ON
+	W.ID_oferty = A.ID_aktualne
+GROUP BY O.Pesel
+GO
+
+CREATE VIEW Pracownik_sprzedane AS
+	SELECT O.Pesel, COUNT(S.ID_sprzedane) AS [Ilosc sprzedanych] FROM Osoby O
+	INNER JOIN Wszystkie_oferty W ON
+	W.Pracownik_obsługujący = O.Pesel
+	LEFT JOIN
+	Sprzedane S ON
+	W.ID_oferty = S.ID_sprzedane
+GROUP BY O.Pesel
+GO
+
+CREATE VIEW Pracownik_niesprzedane AS
+	SELECT O.Pesel, COUNT(N.ID_niesprzedane) AS [Ilosc niesprzedanych] FROM Osoby O
+	INNER JOIN Wszystkie_oferty W ON
+	W.Pracownik_obsługujący = O.Pesel
+	LEFT JOIN
+	Niesprzedane N ON
+	W.ID_oferty = N.ID_niesprzedane
+GROUP BY O.Pesel
+GO
+
+CREATE VIEW Pracownik_statystyki AS
+	SELECT O.Imię, O.Nazwisko, O.Numer_telefonu, Pa.[Ilosc aktualnych], Ps.[Ilosc sprzedanych], Pn.[Ilosc niesprzedanych] FROM Osoby O 
+	INNER JOIN Pracownik_aktualne Pa ON
+	Pa.Pesel = O.Pesel
+	INNER JOIN Pracownik_sprzedane Ps ON
+	Ps.Pesel = O.Pesel
+	INNER JOIN Pracownik_niesprzedane Pn ON
+	Pn.Pesel = O.Pesel
+GO
+
