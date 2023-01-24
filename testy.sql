0522bb3838a7492fd73125cfdeaaf007fe5270b1
KonradSitek <69002597+KonradSitek@users.noreply.github.com>
2023-01-24 23:55:24 +0100

    Update testy.sql

--- testy.sql
+++ testy.sql
@@ -16,6 +16,12 @@ EXEC DodajOgłoszenie 52, '2023-05-01'
 EXEC DodajOgłoszenie 53, '2024-01-02'
 /*/ Dodanie kilku mieszkan pod jednym adresem */
 
+EXEC DodajNieruchomość 'mieszkanie','Aleja Kijowska', 37 , 'Kraków', 61,542000, 0 , 'apartament' , NULL , NULL , NULL, NULL , 1 , 1 , 1 , NULL, NULL, NULL,NULL
+--Brak wymaganego parametru
+
+EXEC DodajNieruchomość 'chalupa','Aleja Kijowska', 37 , 'Kraków', 61,542000, 0 , 'apartament' , NULL , NULL , NULL, NULL , 1 , 1 , 1 , NULL, NULL, NULL,NULL
+--zly typ nieruchomosci
+
 
 EXEC DodajNieruchomość 'działka','Słoneczna', 68 , 'Kraków',100000 ,530123, 0 , 'budowlana' , NULL , NULL , NULL, NULL , NULL , NULL , NULL , 1, 1,1,1
 EXEC DodajOgłoszenie 54,'2023-02-01'
@@ -23,16 +29,38 @@ EXEC DodajOgłoszenie 54,'2023-02-01'
 EXEC DodajNieruchomość 'dom','Sosnowa', 31 , 'Częstochowa', 312,1104001, 1 , 'wolnostojący' , 15 , 2 , 'gaz', NULL , NULL , NULL , NULL , NULL, NULL, NULL,NULL
 EXEC DodajOgłoszenie 55,'2023-02-01'
 
-
-
 EXEC ZarezerwujTerminOglądania '08230862902', 51,  '2023-03-01 16:00:30', '2023-03-01 17:00:00'
 EXEC ZarezerwujTerminOglądania '20082281942', 51,  '2023-03-01 16:50:30', '2023-03-01 18:00:00'
 /*/ Dodanie terminu w którym pracownik jest zajęty */
 
-
 EXEC ZarezerwujTerminOglądania '86072900736', 20,  '2023-03-01 12:00:00', '2023-03-02 18:00:00'
 /*/ Dodanie terminu który trwa zbyt długo */
 
-
 EXEC ZarezerwujTerminOglądania '08230862902', 3,  '2024-03-01 12:00:00', '2024-03-01 13:00:00'
 /*/ Kiedy nie miesci sie w ramach czasowych */
+
+EXEC ZarezerwujTerminOglądania '20082281942',1000,'2023-05-01 15:00', '2023-05-01 16:00'
+--Rezerwacja na nieistniejaca oferete
+
+EXEC Rezerwacja 1,'40071541277','2023-05-01' 
+--Poprawne dodanie rezerwacji
+
+EXEC Rezerwacja 1,'31072069990','2023-05-02' 
+--Proba rezerwacji juz zarezerwowanej oferty
+
+EXEC Rezerwacja 1,'35020376651','2027-05-02' 
+--Niepoprawny przedział czasowy
+
+EXEC Rezerwacja 5,'31072069990','2023-04-10'
+--Dodanie rezerwacji na sprzedane ogłoszenie
+
+EXEC ZakupNieruchomości 1,'31072069990'
+--Proba zakupu nieruchomosci zarezerwowanej przez innego klienta
+
+EXEC ZakupNieruchomości 1,'40071541277'
+--Zakup nieruchomosci zarezerwowanej
+
+
+
+
+
